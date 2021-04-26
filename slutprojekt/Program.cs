6361a85a40d18174e89e2f139116f1c02d81af3b
gustn03 <gustav.perssonwesterberg@gmail.com>
2021-04-26 12:20:32 +0200

    sista

--- slutprojekt/Program.cs
+++ slutprojekt/Program.cs
@@ -3,35 +3,32 @@ using System.Threading;
 
 namespace slutprojekt
 {
+    static class Globals{
+        static public int loop;
+        static public int scen;
+    }
     class Program
     {
-        static void Main(string[] args)
-        {
-            int scen = 0; //vart i spelet man är
-            int loop = 0; //antal gånger man börjat om
-            bool game = true;
-            
-            while (game == true){
-                while(scen == 0){ //start av spelet
+        static void GameLoop(){//start av spelet
+                    Globals.scen = 0;
                     Console.Clear();
                     System.Console.WriteLine("Du vaknar och kollar på klockan");
                     System.Console.WriteLine("Hon är bara 6:23");
                     Thread.Sleep(3000);
                     Console.Clear();
 
-                    if (loop == 1){
+                    if (Globals.loop == 1){
                         System.Console.WriteLine("Trevligt att ses igen!");
                         System.Console.WriteLine("Verkar som att det inte gick så bra där :/");
                         System.Console.WriteLine("Bara att försöka igen");
                         System.Console.WriteLine("Du kommer nog hamna här ett antal gånger tyvärr");
+                        System.Console.WriteLine("Du är nu på loop " + Globals.loop);
                     }
 
-                    else if (loop > 1){
+                    else if (Globals.loop > 1){
                         System.Console.WriteLine("Bara att fortsätta försöka");
-                    }
+                        System.Console.WriteLine("Du är nu på loop " + Globals.loop);
 
-                    else if (loop == 9){
-                        scen = 9;
                     }
 
                     System.Console.WriteLine("Innan vi börjar hade det varit trevligt om jag kunde få ditt samtycke till att spela spelet.");
@@ -46,14 +43,14 @@ namespace slutprojekt
                         System.Console.WriteLine("Glöm inte bort att försöka ha kul!");
                         Thread.Sleep(3000);
                         Console.Clear();
-                        scen++;
+                        Globals.scen++;
                     }
 
                     else if (svar == "stopp"){
                         Console.Clear();
                         System.Console.WriteLine("Om du inte vill så behöver du inte.");
                         System.Console.WriteLine("Hejsvejs :)");
-                        scen = 999;
+                        Globals.scen = 999;
                     }
 
                     else{
@@ -61,13 +58,23 @@ namespace slutprojekt
                         System.Console.WriteLine("Igen, ett \"start\" eller ett \"stopp\"");
                         Thread.Sleep(3000);
                     }
-
-                    while(scen == 1){
+        }
+        static void Main(string[] args){
+                    Globals.scen = 0;
+                    Globals.loop = 0;
+                    GameLoop();
+                    while(Globals.scen == 1){
+                        if (Globals.loop == 9){
+                            break;
+                        }
+                        string val = "null";
+                        string val2 = "null";
+                        string val3 = "null";
                         System.Console.WriteLine("Du kollar på klockan");
                         System.Console.WriteLine("Hon är bara 6:32");
                         System.Console.WriteLine("Skolan börjar inte förrän om X");
                         System.Console.WriteLine("Vill du \"gå upp\" redan nu eller vill du \"ligga kvar\" en stund?");
-                        string val = Console.ReadLine();
+                        val = Console.ReadLine();
                         val.ToLower();
 
                         if (val == "gå upp"){
@@ -79,21 +86,22 @@ namespace slutprojekt
                             System.Console.WriteLine("Din syster springer in i rummet med ett baseboll trä och går berserk på ditt huvud");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 0;
-                            loop++;
+                            Globals.loop++;
+                            GameLoop();
+                            continue;
                         }                       
 
                         else{
                             Console.Clear();
                             System.Console.WriteLine("Du ska lyssna på mig");
-                            scen = 0;
+                            GameLoop();
+                            continue;
                         } 
-
                         System.Console.WriteLine("När du kommer ner och kollar på köksklockan märker du att hon inte är 6:32 utan 9:23");
                         System.Console.WriteLine("Skolan börjar egentligen om y");
                         System.Console.WriteLine("Finns ingen chans att du hinner i tid om du gör frukost");
                         System.Console.WriteLine("Vill du göra \"frukost\" och komma försent eller \"skynda\" till skolan och kanske hinna i tid?");
-                        string val2 = Console.ReadLine();
+                        val2 = Console.ReadLine();
                         val2.ToLower();
 
                         if (val2 == "frukost"){
@@ -105,8 +113,9 @@ namespace slutprojekt
                             System.Console.WriteLine("DU halkar och slår huvudet i kanten på ett bord som inte brukar stå där");
                             Thread.Sleep(5000);
                             Console.Clear();
-                            loop++;
-                            scen = 0;
+                            Globals.loop++;
+                            GameLoop();
+                            continue;
                         }
 
                         else{
@@ -114,26 +123,27 @@ namespace slutprojekt
                             System.Console.WriteLine("Du ska lyssna på mig");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 0;
+                            GameLoop();
+                            continue;
                         }
 
                         System.Console.WriteLine("Efter att du lite skyndat stoppar i dig frukosten går du till hallen och ropar hejdå");
                         Thread.Sleep(2750);
                         System.Console.WriteLine("Konstigt, inget svar tänker du");
-                        if (loop > 2){
+                        if (Globals.loop > 0){
                             System.Console.WriteLine("Känns i alla fall som att din syster borde vara hemma med tanke på det som kanske hände?");
                         }
 
                         System.Console.WriteLine("Du kollar termperaturen ute, 23C");
                         System.Console.WriteLine("Ska du ta på dig något \"lättare\" eller lite \"varmare\"?");
-                        string val3 = Console.ReadLine();
+                        val3 = Console.ReadLine();
                         val3.ToLower();
 
                         if (val3 == "lättare"){
                             System.Console.WriteLine("Du struntar i jacka och går ut med en t-shirt och jeans");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 2;
+                            Globals.scen = 2;
                         }
 
                         else if (val3 == "varmare"){
@@ -141,8 +151,8 @@ namespace slutprojekt
                             System.Console.WriteLine("Du överhettar på bussen");
                             Thread.Sleep(5000);
                             Console.Clear();
-                            loop++;
-                            scen = 0;
+                            GameLoop();
+                            continue;
                         }
 
                         else{
@@ -150,15 +160,18 @@ namespace slutprojekt
                             System.Console.WriteLine("Du ska lyssna på mig");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 0;
+                            GameLoop();
+                            continue;
                         }
                     }
 
-                    while (scen == 2){ //"del 2" av spelet
+                    while (Globals.scen == 2){ //"del 2" av spelet
+                        string val4 = "null";
+                        string val5 = "null";
                         Console.Clear();
                         System.Console.WriteLine("Del 2: På g till skolan");
                         System.Console.WriteLine("På bussen finns alternativet till att \"stå\" eller \"sitta\"");
-                        string val4 = Console.ReadLine();
+                        val4 = Console.ReadLine();
                         val4.ToLower();
 
                         if (val4 == "stå"){
@@ -166,8 +179,9 @@ namespace slutprojekt
                             System.Console.WriteLine("Chauffören väjer av en klippa");
                             Thread.Sleep(5000);
                             Console.Clear();
-                            loop++;
-                            scen = 0;
+                            Globals.loop++;
+                            GameLoop();
+                            continue;
                         }
 
                         else if (val4 == "sitta"){
@@ -179,16 +193,18 @@ namespace slutprojekt
                             System.Console.WriteLine("Du ska lyssna på mig");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 1;
+                            GameLoop();
+                            continue;
                         }
 
                         System.Console.WriteLine("Ingen av de andra resenärerna ger henne deras säte");
                         System.Console.WriteLine("Vill du \"ge upp\" ditt säte eller sitta kvar och \"fortsätta?\"");
-                        string val5 = Console.ReadLine();
+                        val5 = Console.ReadLine();
                         val5.ToLower();
 
                         if (val5 == "ge upp"){
-                            scen = 999;
+                            Globals.scen = 999;
+                            continue;
                         }
 
                         else if (val5 == "fortsätta"){
@@ -200,7 +216,8 @@ namespace slutprojekt
                             System.Console.WriteLine("Du ska lyssna på mig");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 1;
+                            GameLoop();
+                            continue;
                         }
 
                         System.Console.WriteLine("En av dina klasskamrater som hade på sig en t-shirt och jeans var tydligen på samma buss men det såg du inte");
@@ -212,15 +229,18 @@ namespace slutprojekt
                         Console.ReadLine();
                         Thread.Sleep(3000);
                         Console.Clear();
-                        scen = 3;
+                        Globals.scen = 3;
                     }
 
-                    while (scen == 3){ //"Del 3" av spelet
+                    while (Globals.scen == 3){ //"Del 3" av spelet
+                        string val6 = "null";
+                        string val7 = "null";
+                        string val8 = "null";
                         System.Console.WriteLine("Del 3: I skolan");
                         System.Console.WriteLine("Du hinner på något sätt till skolan med z till godo");
                         System.Console.WriteLine("Osäkert om du hinner till klassrummet i tid");
                         System.Console.WriteLine("Vill du \"skynda\" och komma andfådd till klassrummet men kanske i tid eller ta det \"lugnt\" och kanske komma någon minut sent");
-                        string val6 = Console.ReadLine();
+                        val6 = Console.ReadLine();
                         val6.ToLower();
 
                         if (val6 == "skynda"){
@@ -232,8 +252,9 @@ namespace slutprojekt
                             System.Console.WriteLine("Du ramlar på huvudet");
                             Thread.Sleep(5000);
                             Console.Clear();
-                            loop++;
-                            scen = 0;
+                            Globals.loop++;
+                            GameLoop();
+                            continue;
                         }
 
                         else{
@@ -241,17 +262,19 @@ namespace slutprojekt
                             System.Console.WriteLine("Du ska lyssna på mig");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 2;
+                            GameLoop();
+                            continue;
                         }
                         System.Console.WriteLine("Till din förvåning börjar läraren dela ut prov");
                         System.Console.WriteLine("Du har inte riktigt pluggat men känner att du lyssnat under lektionerna");
                         System.Console.WriteLine("Du kommer till en svår fråga som lyder");
                         System.Console.WriteLine("Vill du \"ge upp\" eller \"fortsätta\" med provet?");
-                        string val7 = Console.ReadLine();
+                        val7 = Console.ReadLine();
                         val7.ToLower();
 
                         if (val7 == "ge upp"){
-                            scen = 999;
+                            Globals.scen = 999;
+                            continue;
                         }
 
                         else if (val7 == "fortsätta"){
@@ -263,7 +286,8 @@ namespace slutprojekt
                             System.Console.WriteLine("Du ska lyssna på mig");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 2;
+                            GameLoop();
+                            continue;
                         }
 
                         System.Console.WriteLine("Efter 15 minuter ställer sig halva klassen upp samtidigt");
@@ -275,7 +299,7 @@ namespace slutprojekt
                         System.Console.WriteLine("Du känner att du behöver gå på toa");
                         System.Console.WriteLine("Läraren sover ståendes");
                         System.Console.WriteLine("Vill du \"väcka\" läraren och säga till eller bara \"gå\"?");
-                        string val8 = Console.ReadLine();
+                        val8 = Console.ReadLine();
                         val8.ToLower();
 
                         if (val8 == "väcka"){
@@ -283,7 +307,7 @@ namespace slutprojekt
                             System.Console.WriteLine("De svarar \"Nej du har en fråga kvar, gör den först\"");
                             Thread.Sleep(4000);
                             Console.Clear();
-                            scen = 4;
+                            Globals.scen = 4;
                         }
 
                         else if (val8 == "gå"){
@@ -291,6 +315,10 @@ namespace slutprojekt
                             System.Console.WriteLine("Dörren går i baklås när du tvättar händerna");
                             System.Console.WriteLine("Kranen går inte att stänga av");
                             System.Console.WriteLine("Du drunknar");
+                            Thread.Sleep(5000);
+                            Globals.loop++;
+                            GameLoop();
+                            continue;
                         }
 
                         else{
@@ -298,26 +326,28 @@ namespace slutprojekt
                             System.Console.WriteLine("Du ska lyssna på mig");
                             Thread.Sleep(3000);
                             Console.Clear();
-                            scen = 2;
+                           GameLoop();
+                           continue;
                         }
                     }
 
-                    while (scen == 4){ //"del 4" av spelet
+                    while (Globals.scen == 4){ //"del 4" av spelet
+                        string nio = "null";
                         Console.Clear();
                         System.Console.WriteLine("Sista frågan är");
                         System.Console.WriteLine("\"Vet du vad som händer?\"");
                         System.Console.WriteLine("\"ja\" eller \"nej\"");
-                        string nio = Console.ReadLine();
+                        nio = Console.ReadLine();
                         nio.ToLower();
 
                         if (nio == "ja"){
-                            loop = 9;
-                            scen = 0;
+                            Globals.loop = 9;
+                            GameLoop();
                         }
 
                         else if(nio == "nej"){
                             System.Console.WriteLine("haha");
-                            scen = 5;
+                            Globals.scen = 5;
                         }
 
                         else{
@@ -326,7 +356,7 @@ namespace slutprojekt
                         }
                     }
 
-                    while (scen == 5){ //du vann :)
+                    while (Globals.scen == 5){ //du vann :)
                         Console.Clear();
                         System.Console.WriteLine("Grattis du vann");
                         Thread.Sleep(5000);
@@ -335,7 +365,7 @@ namespace slutprojekt
                         Environment.Exit(0);
                     }
 
-                    while (scen == 999){
+                    while (Globals.scen == 999){
                         Thread.Sleep(3000);
                         Console.Clear();
                         System.Console.WriteLine("Nu är det bara att stänga av");
@@ -356,12 +386,10 @@ namespace slutprojekt
                         Environment.Exit(0);
                     }
 
-                    while (scen == 9){
+                    while (Globals.loop == 9){
                         Console.Write("9");
                     }
-                    }
-                Console.ReadLine();
+                    Console.ReadLine();
+                }
              }
         }
-    }
-}
